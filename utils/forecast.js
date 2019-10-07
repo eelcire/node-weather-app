3159e27a4d446b01ca784bba552b3001e959d9a8
eelcire <eelcire.eelcire@hotmail.com>
2019-10-07 00:02:31 -0500

    1.0.1

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/f960ae3d4dfffee90835ca6f98086a17/' + latitude + ',' + longitude
-    request({ url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service')
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 
